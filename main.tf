608a21b032d1085669f423627340ebfc949861b3
Madhav Kruthiventy <kruthiventym@gmail.com>
2020-06-08 20:32:42 +1200

    add vpcs

--- main.tf
+++ main.tf
@@ -2,6 +2,28 @@ provider "aws" {
     region = "ap-southeast-2"
 }
 
+# VPCs
+
+resource "aws_vpc" "sandbox" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags = {
+    Name = "sandbox"
+  }
+}
+
+resource "aws_vpc" "dev" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags = {
+    Name = "dev"
+  }
+}
+
+# Machines
+
 resource "aws_instance" "cloudera-utility-01" {
   ami           = "ami-04bd9fac35d58053f"        #an AMI that I created
   instance_type = "t2.micro"
